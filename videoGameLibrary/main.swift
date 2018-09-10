04a526d9241ebbd2a5bba2f94104f90e6a35d67a
Cameron Hurt <cameronhurt@Interapts-MacBook-Pro-2.local>
2018-09-10 12:45:04 -0500

    Commiting initial files

---
+++ videoGameLibrary/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  videoGameLibrary
+//
+//  Created by Cameron Hurt on 9/10/18.
+//  Copyright © 2018 Cameron Hurt. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
